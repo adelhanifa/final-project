5ee48f959eecf1b513d18ffd542035dae4d0946c
adelhanifa <adelhanifa@users.noreply.github.com>
2021-01-07 23:35:38 +0100

    edit navbar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './mainCss.css'
+
+// import './mainCss.css'
 import AboutUs from './components/AboutUs'
 import CreateUser from './components/CreateUser';
  import CreateGoals from "./components/CreateGoals";
-import './App.css'
+// import './App.css'
 import IntroPage from "./components/IntroPage";
 function App() {
   return (
     <div className="App">
     <Router>
        <Route path='/' exact component={IntroPage} />
-       <Route path='/aboutus' exact component={AboutUs} />
+       {/* <Route path='/aboutus' exact component={AboutUs} /> */}
        <Route path='/signup' exact component={CreateUser} />
        <Route path='/interestedgoals' exact component={CreateGoals} />
     </Router>
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,21 +1,50 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link as SLink } from 'react-scroll'
+import { Link } from 'react-router-dom'
+import Navbar from 'react-bootstrap/Navbar'
+import { Nav } from 'react-bootstrap';
 
-const Navbar = () => {
+const NavBar = () => {
     return (
         <header className="header float-right" id="header">
+            <Navbar  expand="lg">
+                <div className="container">
+                <Navbar.Brand href="#home">
+                    <div className="logo pull-left" id="logo">
+                        <a href="/" className="scrollto float-left"><img src="assets/img/logo-big1.png" alt="logo" title="" /></a>
+                    </div>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        
+                    </Nav>
+                    <Nav className="mr-auto"> 
+                            <Nav.Link><SLink to="intro" smooth={true}>Home</SLink></Nav.Link>
+                            <Nav.Link><SLink to="about" smooth={true}>About us</SLink></Nav.Link>
+                            <Nav.Link><SLink to="speakers" smooth={true}>Our Team</SLink></Nav.Link>
+                            <Nav.Link><SLink to="contact" smooth={true}>Contact</SLink></Nav.Link>
+                            <Nav.Link><Link to="/login/register" >login/register</Link></Nav.Link>
+                            <Nav.Link><Link to="/sign out" >sign out</Link></Nav.Link>
+                            <Nav.Link className="buy-tickets"><a href="#buy-tickets">Create Goal</a></Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+                </div>
+            </Navbar>
+
             <div className="container">
                 <div className="logo pull-left" id="logo">
                     <a href="/" className="scrollto float-left"><img src="assets/img/logo-big1.png" alt="logo" title="" /></a>
                 </div>
+
                 <nav className="nav-menu-container float-right">
                     <ul className="nav-menu">
-                        <li><Link to="/" />Home</li>
-                        <li><Link to="/aboutus"/>About us</li>
-                        <li><Link to="/teamworker"/>Our Team</li>
-                        <li><Link to="/contact" />Contact</li>
-                        <li><Link to="/login/register" />login/register</li>
-                        <li><Link to="/sign out" />sign out</li>
+                        <li><Link to="/">Home</Link></li>
+                        <li><SLink to="about" smooth={true}>About us</SLink></li>
+                        <li><SLink to="speakers" smooth={true}>Our Team</SLink></li>
+                        <li><SLink to="contact" smooth={true}>Contact</SLink></li>
+                        <li><Link to="/login/register" >login/register</Link></li>
+                        <li><Link to="/sign out" >sign out</Link></li>
                         <li className="buy-tickets"><a href="#buy-tickets">Create Goal</a></li>
                     </ul>
                 </nav>
@@ -26,6 +55,6 @@ const Navbar = () => {
     )
 }
 
-export default Navbar;
+export default NavBar;
 
 
