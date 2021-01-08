35649874d76f7f4e2c5bd9735f9f5a01a5624e93
adelhanifa <adelhanifa@users.noreply.github.com>
2021-01-09 00:45:06 +0100

    fix navbar

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -32,25 +32,52 @@ const NavBar = () => {
                 </div>
             </Navbar> */}
 
-            <div className="container">
-                
+            <nav className="navbar navbar-expand-lg ">
                 <a href="/" className="scrollto float-left ">
                     <img className=" mylogo" src="assets/img/logo/right-red_white.png" alt="logo" title="" />
                 </a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon navbar-light"  >
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </span>
+                </button>
+               
+                <div className="collapse navbar-collapse " id="navbarNav">
+                    <ul className="nav-menu navbar-nav">
+                        <li><Link to="/">Home</Link></li>
+                        <li><SLink to="about" offset={-70} smooth={true}>About us</SLink></li>
+                        <li><SLink to="speakers" offset={-70} smooth={true}>Our Team</SLink></li>
+                        <li><SLink to="contact" offset={-70} smooth={true}>Contact</SLink></li>
+                        <li><Link to="/login/register">login/register</Link></li>
+                        <li><Link to="/sign out" >sign out</Link></li>
+                        <li className="buy-tickets"><a href="#buy-tickets">Create Goal</a></li>
+                    </ul>
+                </div>
+            </nav>
 
-                <nav className="nav-menu-container float-right">
+            
+
+            {/* <div className="container">
+
+                <a href="/" className="scrollto float-left ">
+                    <img className=" mylogo" src="assets/img/logo/right-red_white.png" alt="logo" title="" />
+                </a>
+                
+                <nav className="nav-menu-container float-right" id="navbarNav">
                     <ul className="nav-menu">
                         <li><Link to="/">Home</Link></li>
-                        <li><SLink to="about" smooth={true}>About us</SLink></li>
-                        <li><SLink to="speakers" smooth={true}>Our Team</SLink></li>
-                        <li><SLink to="contact" smooth={true}>Contact</SLink></li>
+                        <li><SLink to="about" offset={-70} smooth={true}>About us</SLink></li>
+                        <li><SLink to="speakers" offset={-70} smooth={true}>Our Team</SLink></li>
+                        <li><SLink to="contact" offset={-70} smooth={true}>Contact</SLink></li>
                         <li><Link to="/login/register">login/register</Link></li>
                         <li><Link to="/sign out" >sign out</Link></li>
                         <li className="buy-tickets"><a href="#buy-tickets">Create Goal</a></li>
                     </ul>
                 </nav>
 
-            </div>
+            </div> */}
         </header>
 
     )
