8feb3e9c34b61c2240de744126409fd3eea831c4
ASREEN <60615177+ASREEN@users.noreply.github.com>
2021-01-08 21:35:35 +0100

    updates

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -6,9 +6,6 @@ import { Nav } from 'react-bootstrap';
 
 const NavBar = () => {
     return (
-<<<<<<< HEAD
-        <header id="header">
-=======
         <header className="header float-right" id="header">
             <Navbar  expand="lg">
                 <div className="container">
@@ -35,23 +32,10 @@ const NavBar = () => {
                 </div>
             </Navbar>
 
->>>>>>> 75b673020f6210b77a6d159c03b37804e2437378
             <div className="container">
                 <div className="logo pull-left" id="logo">
                     <a href="/" className="scrollto"><img src="assets/img/logo-big1.png" alt="logo" title="logo" /></a>
                 </div>
-<<<<<<< HEAD
-                
-                <nav className="nav-menu-container">
-                    <ul className="nav-menu">
-                        <li><Link to="/" className="menu-active"/>Home</li>
-                        <li><Link to="/aboutus"/>About us</li>
-                        <li><Link to="/teamworker"/>Our Team</li>
-                        <li><Link to="/contact" />Contact</li>
-                        <li><Link to="/login/register" />login/register</li>
-                        <li><Link to="/sign out" />sign out</li>
-                        <li className="buy-tickets"><a href="/create/goal">Create Goal</a></li>
-=======
 
                 <nav className="nav-menu-container float-right">
                     <ul className="nav-menu">
@@ -59,10 +43,9 @@ const NavBar = () => {
                         <li><SLink to="about" smooth={true}>About us</SLink></li>
                         <li><SLink to="speakers" smooth={true}>Our Team</SLink></li>
                         <li><SLink to="contact" smooth={true}>Contact</SLink></li>
-                        <li><Link to="/login/register" >login/register</Link></li>
+                        <li><Link to="/login/register">login/register</Link></li>
                         <li><Link to="/sign out" >sign out</Link></li>
                         <li className="buy-tickets"><a href="#buy-tickets">Create Goal</a></li>
->>>>>>> 75b673020f6210b77a6d159c03b37804e2437378
                     </ul>
                 </nav>
 
