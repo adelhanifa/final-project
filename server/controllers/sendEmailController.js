ce5fc52581cfcba7b9fe1c778e13a90a848ba570
adelhanifa <adelhanifa@users.noreply.github.com>
2021-01-09 23:55:51 +0100

    add send email (configure , reset contact)

---
+++ server/controllers/sendEmailController.js
@@ -0,0 +1,82 @@
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.EmailAPI);
+const User = require('../models/User');
+
+// Send Email Contact form
+exports.contactForm = (req, res) => {
+    req.body.dateFormat = new Date()
+
+    console.log('******************************************');
+    console.log('contactSendEmail', req.body);
+    // req.body ={ req.body.name, req.body.email, req.body.subject, req.body.message, req.body.dateFormat }
+
+    const msg = {
+        to: 'adelhanifh@gmail.com',
+        from: 'adelhanifh@gmail.com',
+        subject: req.body.subject + ' | ' + req.body.name,
+        html: `
+            <div>
+                <h1>Welcome to On Target</h1>
+                <h2>Hi I am ${req.body.name},</h2>
+                <h2>I send to you this E-mail because:</h2>
+                <div style="font-weight: 600; text-align: justify;">${req.body.message}</div>
+                <h2>Please reply to my message as soon as possible.</h2>
+                <h3>My email is: ${req.body.email}</h3>
+                <h4>Thank you</h4>
+                <p>This email was sent at ${req.body.dateFormat}</p>
+            </div>`
+    }
+    
+    sgMail.send(msg, (err, info) => {
+        if (err) {
+            console.log('Email not Sent', err)
+            res.send('Email not Sent', err)
+        } else {
+            console.log('Your message has been sent. Thank you!')
+            res.send('Your message has been sent. Thank you!')
+        }
+    })
+}
+
+// Send Email reset password
+exports.resetPassword = (req, res) => {
+    req.body.dateFormat = new Date()
+
+    console.log('******************************************');
+    console.log('reset Password', req.body);
+
+    User.findOne({ email: req.body.email }, (err, data) => {
+        if (data == null) {
+            res.send('No account matches this email: ' + req.body.email);
+        }
+        else {
+            const msg = {
+                to: 'adelhanifh@gmail.com',
+                from: 'adelhanifh@gmail.com',
+                subject: 'Reset your Password | ' + data.firstName + ' ' + data.lastName,
+                html: `
+                <div>                
+                    <h1>Welcome to On Target</h1>
+                    <h2>Hello ${data.firstName} ${data.lastName}</h2>
+                    <h2>Please click below to rest the Password</h2>
+                    <div>
+                        <a href="http://localhost:3000/user/reset/${user.id}" type="button">reset your password now</a>
+                    </div>
+                    <h3>If you still have problem <a href="http://localhost:3000">contact us</a> </h3>
+                    <h4>Thank you</h4>
+                    <p>This email was sent at ${req.body.dateFormat}</p>
+                </div>`
+            }
+            console.log('msg', msg);
+            sgMail.send(msg, (err, info) => {
+                if (err) {
+                    console.log('Email not Sent', err)
+                    res.send('Email not Sent', err)
+                } else {
+                    console.log('Your message has been sent. Thank you!')
+                    res.send('Reset Password email has been sent to you. Thank you!')
+                }
+            })
+        }
+    })
+}
--- server/controllers/usersController.js
+++ server/controllers/usersController.js
@@ -1,3 +1,5 @@
+const sgMail = require('@sendgrid/mail')
+sgMail.setApiKey(process.env.EmailAPI);
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 
@@ -8,7 +10,7 @@ exports.findAllUsers = (req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 }
 
-//create a new user
+//create a new user with send email
 exports.createNewUser = (req, res) => {
     console.log('******************************************');
 
@@ -21,7 +23,38 @@ exports.createNewUser = (req, res) => {
 
         const newUser = new User(req.body);
         newUser.save()
-        .then(user => res.json(user))
+        .then(user => {
+            let date = new Date()
+
+            const msg = {
+                to: 'adelhanifh@gmail.com',
+                from: 'adelhanifh@gmail.com',
+                subject: user.firstName + ' ' + user.lastName + ' | confirm email address',
+                html: `
+                <div>                
+                    <h1>Welcome to On Target</h1>
+                    <h2>Hello ${user.firstName} ${user.lastName}</h2>
+                    <h2>Thank you for registering on our website</h2>
+                    <h2>Please confirm your account by clicking on the button below</h2>
+                    <div>
+                        <a href="http://localhost:3000/user/confirm/${user.id}" type="button">reset your password now</a>
+                    </div>
+                    <h3>If you still have problem <a href="http://localhost:3000">contact us</a> </h3>
+                    <h4>Thank you</h4>
+                    <p>This email was sent at ${date}</p>
+                </div>`
+            }
+            console.log('msg', msg);
+            sgMail.send(msg, (err, info) => {
+                if (err) {
+                    console.log('Email not Sent', err)
+                    res.send({status: 'new user created but Email not Sent', userinfo: user, errMSG: err})
+                } else {
+                    console.log('Your message has been sent. Thank you!')
+                    res.send({status: 'new user created and confirm email has been sent to you', userinfo: user, errMSG: err})
+                }
+            })
+        })
         .catch(err => res.status(400).json('Error: ' + err));
     }); 
 }
