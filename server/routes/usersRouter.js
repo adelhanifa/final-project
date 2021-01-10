e4b048867173d89463836f9267dfdc8800868ced
adelhanifa <adelhanifa@users.noreply.github.com>
2021-01-10 19:26:39 +0100

    add multer img upload

--- server/routes/usersRouter.js
+++ server/routes/usersRouter.js
@@ -1,8 +1,33 @@
 const router = require('express').Router();
 const controller = require('../controllers/usersController');
+const multer = require('multer');
+
+//multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => { cb(null, './public/img'); },
+    filename: (req, file, cb) => {
+        let fileName = file.originalname.split('.');
+        cb(null, file.fieldname + '-' + Date.now() + '.' + fileName[fileName.length-1])
+    }
+    // filename: function (req, file, res) {
+    //     res(null, file.originalname.toLowerCase().split(' ').join('-'))
+    // },
+});
+
+var upload = multer({
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+        if (file.mimetype == "image/png" || file.mimetype == "image/jpg" || file.mimetype == "image/jpeg") {
+            cb(null, true);
+        } else {
+            cb(null, false);
+            return cb(new Error('Only .png, .jpg and .jpeg format allowed!'));
+        }
+    }
+});
 
 router.get('/', controller.findAllUsers);
-router.post('/create', controller.createNewUser);
+router.post('/create',  upload.single('profileImg'), controller.createNewUser);
 router.post('/log-in', controller.loginUser)
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,9 +1,9 @@
-const express = require('express')
-const cors = require('cors')
+const express = require('express');
+const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 //DATABASE
 const dbLink = process.env.DBLink;
